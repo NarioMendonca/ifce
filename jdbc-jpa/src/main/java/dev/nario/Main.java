b5d40fb6ec0364f5a5f38ee671b4168b5cba4dde
NarioMendonca <nario.sousa2014@gmail.com>
2026-06-22 23:16:05 -0300

    jdbc-jpa: test DAOs and entities

--- jdbc-jpa/src/main/java/dev/nario/Main.java
+++ jdbc-jpa/src/main/java/dev/nario/Main.java
@@ -1,7 +1,50 @@
 package dev.nario;
 
+import java.time.LocalDate;
+
+import dev.nario.db.DAOFactory;
+import dev.nario.db.DepartmentDAO;
+import dev.nario.db.SellerDAO;
+import dev.nario.entities.Department;
+import dev.nario.entities.Seller;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SellerDAO sellerDAO = DAOFactory.createSellerDAO();
+        DepartmentDAO departmentDAO = DAOFactory.createDepartmentDAO();
+        
+        Department salesDepartment = new Department(null, "Vendas");
+        Department rhDepartment = new Department(null, "Rhh");
+        salesDepartment = departmentDAO.insert(salesDepartment);
+        rhDepartment = departmentDAO.insert(rhDepartment);
+        rhDepartment.setName("RH");
+        departmentDAO.update(rhDepartment);
+
+        Seller sellerJoao = new Seller(
+            null,
+            "João Silva",
+            "joao@gmail.com", 
+            LocalDate.of(2004, 3, 7), 
+            3692
+        );
+        sellerJoao.setDepartment(salesDepartment);
+        sellerDAO.insert(sellerJoao);
+
+        Seller sellerPereira = new Seller(
+            null,
+            "Pereira Alves",
+            "pereiraA@gmail.com", 
+            LocalDate.of(1997, 7, 2), 
+            6180
+        );
+        sellerPereira.setDepartment(rhDepartment);
+        sellerPereira = sellerDAO.insert(sellerPereira);
+        sellerPereira.setEmail("pereira2@gmail.com");
+        sellerDAO.update(sellerPereira);
+
+
+
+        System.out.println("Cadastrados com sucesso!");
+
     }
 }
